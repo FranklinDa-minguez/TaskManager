8223aa48fd89d1db227fe0656cd94e6080531c06
Franklin Dominguez <f.e.dominguez157@gmail.com>
2024-10-10 03:33:31 -0400

    backend for login

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, session
 from flask_session import Session
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS, cross_origin
 from config import ApplicationConfig
 from models import db, User
 
@@ -8,6 +9,7 @@ app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
+cors = CORS(app, supports_credentials=True)
 server_session = Session(app)   
 db.init_app(app)
 
